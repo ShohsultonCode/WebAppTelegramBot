f71fb107133dfda754c5796ab722b4af42752b76
Shohsulton Ummatov <108561261+ShohsultonCode@users.noreply.github.com>
2024-06-07 13:35:32 +0500

    some

--- bot.js
+++ bot.js
@@ -2,17 +2,16 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "7333536576:AAEy1MtDXijiY1NYEEMCFv3L73cpnDbWrek";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://web-app-tme-interface.vercel.app/";
+const adminLink = "https://tg-app-admin.vercel.app";
+const userLink = "https://web-app-tme-interface.vercel.app";
 
 bot.start((ctx) => {
   const userId = ctx.from.id;
-  const webAppUrl = `${web_link}?user_id=${userId}`;
-  ctx.reply("Welcome :)))))", {
+  const webAppUrl = userId === 5171708849 ? adminLink : `${userLink}?user_id=${userId}`;
+
+  ctx.reply("Welcome", {
     reply_markup: {
       keyboard: [[{ text: "web app", web_app: { url: webAppUrl } }]],
     },
   });
 });
-
-bot.launch();
-  
